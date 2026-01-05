3624e46215e7e995a7b78464fb3bf6fe7bb82ff9
unknown <yongpro0526@gmail.com>
2026-01-05 17:42:33 +0900

    2026-01-05 유저_틋-01-05_유저엔티티수정

--- src/main/java/com/diary/entity/User.java
+++ src/main/java/com/diary/entity/User.java
@@ -5,16 +5,16 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Table(name = "users")
+@Entity // 이 클래스는 JPA 메서드임을 선언
+@Getter // 모든 필드에 Getter메서드 자동 생성
+@Setter // 모든 필드에 Setter메서드 자동 생성
+@NoArgsConstructor // 기본 생성자 자동 생성
+@AllArgsConstructor // 모든 필드를 받는 생성자 자동 생성
+@Builder // 빌더패턴 자동 생성
+@Table(name = "users") // 테이블 이름을 users로 설정.(User는 예약어이기 때문에 테이블 이름으로 사용 불가능)
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT, 1씩 늘어남.
     private Long id;
 
     @Column(unique = true, nullable = false)
@@ -24,5 +24,18 @@ public class User {
     private String password;
 
     private String name;
+
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() { // 회원가입 시
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();  // 처음엔 같게
+    }
+
+    @PreUpdate
+    protected void onUpdate() { // 회원정보 수정 시
+        updatedAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/com/diary/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.diary.repository;
+
+public class UserRepository {
+}
